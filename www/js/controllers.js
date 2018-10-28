d0b9f3a0ef6763d52680e723b90f721004711dd8
முகிலன் Muhelen <muhelen@omtamil.com>
2018-10-29 00:04:59 +0800

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,66 @@
+angular.module('app.controllers', [])
+  
+.controller('page1Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page8Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page5Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('homepageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page7Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,63 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('page1', {
+    url: '/page1',
+    templateUrl: 'templates/page1.html',
+    controller: 'page1Ctrl'
+  })
+
+  .state('page2', {
+    url: '/page2',
+    templateUrl: 'templates/page2.html',
+    controller: 'page2Ctrl'
+  })
+
+  .state('page8', {
+    url: '/page8',
+    templateUrl: 'templates/page8.html',
+    controller: 'page8Ctrl'
+  })
+
+  .state('page3', {
+    url: '/page3',
+    templateUrl: 'templates/page3.html',
+    controller: 'page3Ctrl'
+  })
+
+  .state('2', {
+    url: '/page4',
+    templateUrl: 'templates/2.html',
+    controller: '2Ctrl'
+  })
+
+  .state('page5', {
+    url: '/page5',
+    templateUrl: 'templates/page5.html',
+    controller: 'page5Ctrl'
+  })
+
+  .state('homepage', {
+    url: '/page6',
+    templateUrl: 'templates/homepage.html',
+    controller: 'homepageCtrl'
+  })
+
+  .state('page7', {
+    url: '/page7',
+    templateUrl: 'templates/page7.html',
+    controller: 'page7Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page6')
+
+
+});
